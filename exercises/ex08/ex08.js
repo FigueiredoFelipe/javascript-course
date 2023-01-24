14fe0d2a205773dfa9877c14f7a591a893a1e697
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-01-24 19:40:50 -0300

    feat: ex08-ex09

---
+++ exercises/ex08/ex08.js
@@ -0,0 +1,7 @@
+var vel = 60.1
+var velMax = 60
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60){
+    console.log(`Você foi multado por trafegar em velocidade superior a ${velMax}km/h.`)
+} else {
+console.log(`Dirija sempre usando cinto de segurança!`)}
---
+++ exercises/ex09/ex09.js
@@ -0,0 +1,9 @@
+var country = 'Brasil'
+console.log(`Se nasceu no ${country}, `)
+if (country == 'Brasil'){
+    console.log('é brasileiro.')
+} else if (country == 'EUA'){
+    console.log('é americano.')
+} else {
+    console.log('É estrangeiro')
+}
