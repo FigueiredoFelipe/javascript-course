25ff5dc3725f1701897ff297cd2521b13b3f8623
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-01-27 16:58:53 -0300

    feat: exercises

--- exercises/ex16/functions.js
+++ exercises/ex16/functions.js
@@ -2,30 +2,15 @@ const userInput = 100
 
 
 function parImp(userInput) {
-    if (userInput % 2 === 0) {
+    if (userInput%2 === 0) {
         console.log(`even`)
-    } else if (userInput % 2 !== 0){
-        console.log(`odd`)
+        return 'even'
     } else {
-        return
+        console.log(`odd`)
+        return 'odd'
     }
 }
 
-parImp()
+let res = parImp(userInput)
 
-
-
-Lápis De Cor Longo 36 Cores Triangular Serie Ouro - 30
-
-/*
-https://produto.mercadolivre.com.br/MLB-3097225926-lapis-de-cor-longo-36-cores-triangular-serie-ouro-_JM#position=1&search_layout=grid&type=item&tracking_id=7d2d3b01-4078-4e7f-bd7c-a2cfcb177b80
-
-30+25 = 55
-
-
-***
-https://produto.mercadolivre.com.br/MLB-3099499408-caixa-lapis-de-cor-com-36-cores-corpo-triangular-leoleo-_JM#position=2&search_layout=grid&type=item&tracking_id=7d2d3b01-4078-4e7f-bd7c-a2cfcb177b80
-
-33 + 14 = 47
-
-/*
+console.log(res)
