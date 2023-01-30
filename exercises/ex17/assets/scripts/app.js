de66df44e1a4da8d8b60ce09a788aad68898f39c
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-01-30 11:40:20 -0300

    fix: ex17 - bgcolor

--- exercises/ex17/assets/scripts/app.js
+++ exercises/ex17/assets/scripts/app.js
@@ -2,15 +2,16 @@ const monthNames = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
 const time = new Date()
 const displayTime = document.getElementById('time-now')
 // const hour = parseInt(Math.random() * 23) for test.
-// const hour = '1'
-const hour = time.getHours()
+const hour = '6'
+// const hour = time.getHours()
 const day = time.getDate()
 const month = monthNames[time.getMonth()]
 const year = time.getFullYear()
 
 const img = document.getElementsByTagName('img')
 const allImages = img.innerHTML
-const background = document.getElementById('bg')
+let background = document.getElementById('bg')
+let backColor = background.style
 
 displayTime.innerText = `Agora são ${hour} horas do dia ${day} do mês de ${month} de ${year}.`
 
@@ -18,7 +19,7 @@ displayTime.innerText = `Agora são ${hour} horas do dia ${day} do mês de ${mon
 
 if (hour >= 6 && hour < 11) {
     document.getElementById("image").src = "./assets/images/01.png";
-    background.style.backgroundColor = rgb(243, 223, 171);
+    background.style.backgroundColor = '#b5d1e3';
 
 } else if (hour >= 11 && hour < 15) {
     document.getElementById("image").src = "./assets/images/02.png";
