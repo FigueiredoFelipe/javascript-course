a60ce9376fb1e50a63a81d40c176bad7b2fed0f1
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-02-01 00:03:57 -0300

    feat: user data display in the browser

--- exercises/ex18-ageverifier/assets/scripts/app.js
+++ exercises/ex18-ageverifier/assets/scripts/app.js
@@ -4,110 +4,124 @@ const userMale = document.getElementsByName('gender')[0]
 const userFemale = document.getElementsByName('gender')[1]
 const userOther = document.getElementsByName('gender')[2]
 const button = document.getElementsByClassName('handle-button')[0]
+const displayData = document.getElementById('display-data')
 
 const femaleStage = ["Baby girl", "Little girl", "Girl", "Teenage girl", "Young woman", "Woman", "Older woman", "Elderly woman"]
 
 const maleStage = ["Baby boy", "Little boy", "Boy", "Teenage boy", "Young man", "Man", "Older man", "Elderly man"]
 
 
+
+displayData.innerText = `User Data`
+
+
 const verify = () => {
 
     let userAge = currentYear - birthYear.value
+    let userData = displayData.innerHTML
 
     /*Male*/
 
-    if (userMale.checked === true && userAge == 0){
+    if (userMale.checked === true && userAge == 0) {
+
+        displayData.innerText = `The user informed us he is a ${maleStage[0]} and will complete ${++userAge}-year-old next year.`
+
         console.log(`The user informed us he is a ${maleStage[0]} and will complete ${++userAge}-year-old next year.`)
     }
 
-else if (userMale.checked === true && userAge >0 && userAge <=1){
-        console.log(`The user informed us he is a ${maleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`)
-    }
-    
-    else if (userMale.checked === true && userAge > 1 && userAge <=3){
+    else if (userMale.checked === true && userAge > 0 && userAge <= 1) {
+
+        displayData.innerText = `The user informed us he is a ${maleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`
+
         console.log(`The user informed us he is a ${maleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userMale.checked === true && userAge > 3 && userAge <=8){
+    else if (userMale.checked === true && userAge > 1 && userAge <= 3) {
+        displayData.innerText = `The user informed us he is a ${maleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`
+
+        console.log(`The user informed us he is a ${maleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`)
+    }
+
+    else if (userMale.checked === true && userAge > 3 && userAge <= 8) {
         console.log(`The user informed us he is a ${maleStage[1]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userMale.checked === true && userAge > 8 && userAge <=13){
+    else if (userMale.checked === true && userAge > 8 && userAge <= 13) {
         console.log(`The user informed us he is a ${maleStage[2]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userMale.checked === true && userAge > 13 && userAge <=19){
+    else if (userMale.checked === true && userAge > 13 && userAge <= 19) {
         console.log(`The user informed us he is a ${maleStage[3]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userMale.checked === true && userAge > 19 && userAge <=35){
+    else if (userMale.checked === true && userAge > 19 && userAge <= 35) {
         console.log(`The user informed us he is a ${maleStage[4]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userMale.checked === true && userAge > 35 && userAge <=55){
+    else if (userMale.checked === true && userAge > 35 && userAge <= 55) {
         console.log(`The user informed us he is a ${maleStage[5]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userMale.checked === true && userAge > 55 && userAge <=65){
+    else if (userMale.checked === true && userAge > 55 && userAge <= 65) {
         console.log(`The user informed us he is a ${maleStage[6]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userMale.checked === true && userAge > 65 && userAge <=120){
+    else if (userMale.checked === true && userAge > 65 && userAge <= 120) {
         console.log(`The user informed us he is a ${maleStage[7]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
     /*Female*/
 
-    else if (userFemale.checked === true && userAge == 0){
+    else if (userFemale.checked === true && userAge == 0) {
         console.log(`The user informed us she is a ${femaleStage[0]} and will complete ${++userAge}-year-old next year.`)
     }
-    
-    else if (userFemale.checked === true && userAge >0 && userAge <=1){
-        console.log(`The user informed us she is a ${femaleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`)
-    }
-    
-    else if (userFemale.checked === true && userAge > 1 && userAge <=3){
+
+    else if (userFemale.checked === true && userAge > 0 && userAge <= 1) {
         console.log(`The user informed us she is a ${femaleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userFemale.checked === true && userAge > 3 && userAge <=8){
+    else if (userFemale.checked === true && userAge > 1 && userAge <= 3) {
+        console.log(`The user informed us she is a ${femaleStage[0]} and will complete ${userAge}-years-old by the end of the current year.`)
+    }
+
+    else if (userFemale.checked === true && userAge > 3 && userAge <= 8) {
         console.log(`The user informed us she is a ${femaleStage[1]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userFemale.checked === true && userAge > 8 && userAge <=13){
+    else if (userFemale.checked === true && userAge > 8 && userAge <= 13) {
         console.log(`The user informed us she is a ${femaleStage[2]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userFemale.checked === true && userAge > 13 && userAge <=19){
+    else if (userFemale.checked === true && userAge > 13 && userAge <= 19) {
         console.log(`The user informed us she is a ${femaleStage[3]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userFemale.checked === true && userAge > 19 && userAge <=35){
+    else if (userFemale.checked === true && userAge > 19 && userAge <= 35) {
         console.log(`The user informed us she is a ${femaleStage[4]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userFemale.checked === true && userAge > 35 && userAge <=55){
+    else if (userFemale.checked === true && userAge > 35 && userAge <= 55) {
         console.log(`The user informed us she is a ${femaleStage[5]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userFemale.checked === true && userAge > 55 && userAge <=65){
+    else if (userFemale.checked === true && userAge > 55 && userAge <= 65) {
         console.log(`The user informed us she is a ${femaleStage[6]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    else if (userFemale.checked === true && userAge > 65 && userAge <=120){
+    else if (userFemale.checked === true && userAge > 65 && userAge <= 120) {
         console.log(`The user informed us she is a ${femaleStage[7]} and will complete ${userAge}-years-old by the end of the current year.`)
     }
 
-    
+
     /*Other*/
 
-    else if (userOther.checked === true && userAge >=0 && userAge <= 120){
+    else if (userOther.checked === true && userAge >= 0 && userAge <= 120) {
         console.log(`The user will complete ${userAge}-years-old by the end of the current year.`)
     }
 
 
 
-    
+
 
     else {
         alert('Please insert a valid Birth Date and Gender.')
